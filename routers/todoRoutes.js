67374519b139b6be0118f7155e8a805593df7b39
Kabir Hossain <104571104+kobir1989@users.noreply.github.com>
2022-11-27 00:42:02 +0600

    Added - PUT, DELETE GET endpoint added

--- routers/todoRoutes.js
+++ routers/todoRoutes.js
@@ -1,8 +1,17 @@
 const router = require("express").Router();
 const Todo = require("../model/Todo");
 
-router.get("/todos", (req, res) => {});
+//Todo GET endpoint
+router.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await Todo.find();
+    res.status(200).json(allTodos);
+  } catch (error) {
+    res.status(500).json({ errorMessage: "Something went wrong" });
+  }
+});
 
+//Todo POST endpoint
 router.post("/todos", async (req, res) => {
   const { title, todo } = req.body;
   console.log(req.user);
@@ -23,8 +32,41 @@ router.post("/todos", async (req, res) => {
   }
 });
 
-router.delete("/todos/:id", (req, res) => {});
+//Todo DELETE endpoint
+router.delete("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const findTodo = await Todo.findOne({ id });
+    if (!findTodo) {
+      return res.status(401).json({ errorMessge: "Todo does not exists" });
+    }
+    await findTodo.delete();
+    res.status(200).send();
+  } catch (error) {
+    res.status(500).json({ errorMessage: "Something went wrong" });
+  }
+});
 
-router.put("/todos/:id", (req, res) => {});
+//Todo PUT endpoint
+router.put("/todos/:id", async (req, res) => {
+  const { title, todo } = req.body;
+  const { id } = req.params;
+  try {
+    if (!(title && todo)) {
+      return res.status(401).json({ errorMessge: "All the feilds are required" });
+    }
+    const originalTodo = await Todo.findOne({ id });
+    if (!originalTodo) {
+      return res.status(401).json({ errorMessge: "Todo does not exists" });
+    }
+    originalTodo.title = title;
+    originalTodo.todo = todo;
+    const updatedTodo = await originalTodo.save();
+    res.status(200).json(updatedTodo);
+  } catch (error) {
+    res.status(500).json({ errorMessage: "Something went wrong" });
+    console.log(error)
+  }
+});
 
 module.exports = router;
